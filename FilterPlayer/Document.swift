e1dc91bd59b6c43cf38bfaa536091b4c488221a2
Leo Liu <leoliu8@deloitte.com>
2019-12-31 08:20:39 -0500

    Initial Commit

---
+++ FilterPlayer/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  FilterPlayer
+//
+//  Created by Leo on 12/31/19.
+//  Copyright © 2019 Super Art Software. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
---
+++ FilterPlayer/DocumentView.swift
@@ -0,0 +1,27 @@
+//
+//  DocumentView.swift
+//  FilterPlayer
+//
+//  Created by Leo on 12/31/19.
+//  Copyright © 2019 Super Art Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct DocumentView: View {
+    var document: UIDocument
+    var dismiss: () -> Void
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("File Name")
+                    .foregroundColor(.secondary)
+
+                Text(document.fileURL.lastPathComponent)
+            }
+
+            Button("Done", action: dismiss)
+        }
+    }
+}
